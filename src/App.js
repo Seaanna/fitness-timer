1622322c71d8cfe991abfe8c155df2e97616a460
SeaAnna <seaanna.thompson@gmail.com>
2020-04-22 20:57:31 -0700

    clickable buttons and a timer starting from 0 that starts stops and resets that uses react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Timer from "./components/timer";
+import StopWatch from "./components/stopWatch";
 import './App.css';
 
 function App() {
   return (
     <div className="container">
-      <Timer/>
+      <Router>
+        <Switch>
+          <Route path="/stopwatch">
+            <StopWatch/>
+          </Route>
+          <Route path="/">
+            <Timer/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/stopWatch.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { formatSeconds } from "../helpers/timer";
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  const reset = () => {
+    setIsActive(false)
+    setSeconds(0)
+  };
+
+  return (
+    <div>
+      <p className="timer_text">
+        { formatSeconds(seconds) }
+      </p>
+      <div className="button_grid">
+        <button className="start" onClick={() => setIsActive(true)}>
+          Start
+        </button>
+        <button className="stop" onClick={() => setIsActive(false)}>
+          Stop
+        </button>
+        <button className="reset" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
